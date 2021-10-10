f04ef8a6013b43686c94ef2eb8267761dda3c5ed
ethanschwartz225 <ethans@bu.edu>
2021-10-10 12:50:55 +0300

    Week_2

---
+++ Week_2/Day_6/Exercises/Exercise1.js
@@ -0,0 +1,29 @@
+//Guesses
+    let a = 534 //CORRECT
+    let b = 1 //CORRECT
+    let c = 0 //CORRECT
+    let d = 5 //CORRECT
+    let e = "JavaScript" //CORRECT
+    let f = "  " //CORRECT
+    let g = " " //WRONG
+    let h = true //WRONG
+    let i = false //WRONG
+    let j = false //WRONG
+    let k = NaN //WRONG
+    let l = -1 //CORRECT
+    let m = NaN //CORRECT
+
+    5 + "34"
+    5 - "4"
+    10 % 5
+    5 % 10
+    "Java" + "Script"
+    " " + " "
+    " " + 0      // ' 0'
+    true + true // 2
+    true + false // 1
+    false + true // 1
+    false - true // -1
+    3 - 4
+    "Bob" - "bill"
+
